763a818874f9612c34a98d3a4b125a3eae9f50ff
WangFangfang <fangfangliana@gmail.com>
2020-09-13 23:32:25 +0800

    replace AT_CHECK with TORCH_CHECK for high pytorch version

--- curve/ops/geometry/src/iou_cuda.cpp
+++ curve/ops/geometry/src/iou_cuda.cpp
@@ -1,7 +1,6 @@
 #include <torch/extension.h>
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
 
 at::Tensor iou_cuda(const at::Tensor boxes);
 
